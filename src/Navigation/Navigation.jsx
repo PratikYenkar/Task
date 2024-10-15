46d3f0e403392a1025aeca59483a509577387acb
Pratik Yenkar <pratikyenkar09@gmail.com>
2024-10-16 01:12:47 +0530

    my project

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -14,7 +14,8 @@ const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login"
+      screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Reset" component={ResetInfo} />
         <Stack.Screen name="SetPassword" component={ResetPassword} />
